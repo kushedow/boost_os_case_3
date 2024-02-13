7f825d306d9790219a6623d6dda2d6f839a94772
Aleksandr Sundeev <navigator97@mail.ru>
2024-02-13 14:02:37 +0700

    tests is working w/o errors

--- src/classes.py
+++ src/classes.py
@@ -2,7 +2,7 @@ class Category:
     """Класс категории"""
     count_of_category = 0
 
-    def __init__(self, name: str, description: str, goods):
+    def __init__(self, name: str, description: str, goods: list):
         self.name = name
         self.description = description
         self.goods = goods
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -4,21 +4,22 @@ from src.classes import Category, Product
 
 @pytest.fixture
 def class_category():
-    return Category('Смартфоны', 'Смартфоны', 'fhfgh')
-    # {
-    #     "name": "Samsung Galaxy C23 Ultra",
-    #     "description": "256GB, Серый цвет, 200MP камера",
-    #     "price": 180000.0,
-    #     "quantity": 5
-    # })
+    return Category('Смартфоны',
+                    'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни',
+                    [{
+                        "name": "Samsung Galaxy C23 Ultra",
+                        "description": "256GB, Серый цвет, 200MP камера",
+                        "price": 180000.0,
+                        "quantity": 5
+                    }])
 
 
 def test_category_init(class_category):
     assert class_category.name == 'Смартфоны'
-    assert class_category.description == 'Смартфоны'
-    # assert class_category.goods == [{
-    #                     "name": "Samsung Galaxy C23 Ultra",
-    #                     "description": "256GB, Серый цвет, 200MP камера",
-    #                     "price": 180000.0,
-    #                     "quantity": 5
-    #                 }]
+    assert class_category.description == 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни'
+    assert class_category.goods == [{
+                        "name": "Samsung Galaxy C23 Ultra",
+                        "description": "256GB, Серый цвет, 200MP камера",
+                        "price": 180000.0,
+                        "quantity": 5
+                    }]
