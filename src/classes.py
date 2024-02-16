deb232432d2aae08d851ae171cf8028135d553bd
Aleksandr Sundeev <navigator97@mail.ru>
2024-02-16 20:17:24 +0700

    add docstrings to classes & add def new_goods

--- src/classes.py
+++ src/classes.py
@@ -12,12 +12,19 @@ class Category:
         Category.count_of_category += 1
 
     def add_list_goods(self):
+        """
+        добавление товаров в категорию
+        """
         return self.list_goods.append(self.__goods)
 
     def print_goods(self):
+        """
+        геттер, который выводит список товаров
+        """
         for i in range(0, len(self.list_goods)):
             for j in range(0, len(self.list_goods[i])):
-                print(f'{self.list_goods[i][j]['name']}, {int(self.list_goods[i][j]['price'])} руб. Остаток: {self.list_goods[i][j]['quantity']} шт.')
+                print(
+                    f'{self.list_goods[i][j]['name']}, {int(self.list_goods[i][j]['price'])} руб. Остаток: {self.list_goods[i][j]['quantity']} шт.')
 
     def __repr__(self):
         return f"Category: ('{self.name}', {self.description}, '{self.__goods}', 'общее количество категорий={self.count_of_category} количество уникальных продуктов={self.unique_goods}')"
@@ -26,6 +33,7 @@ class Category:
 class Product:
     """Класс продукт"""
     count_of_products = 0
+    new_good = []
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
@@ -34,6 +42,13 @@ class Product:
         self.quantity = quantity
 
         Product.count_of_products += 1
+
+    @classmethod
+    def new_goods(cls, goods):
+        """
+        создание товара
+        """
+        cls.new_good = goods
 
     def __repr__(self):
         return f"Product: ('{self.name}', {self.description}, '{self.price}', 'Количество продуктов в наличии={self.quantity}')"
