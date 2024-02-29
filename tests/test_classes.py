429892a5c9fe99cbc16ffa656d14a308235edf06
Gleb Kushedow <kushedow@gmail.com>
2024-02-29 15:40:31 +0400

    Update test_classes.py

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -2,9 +2,7 @@ import pytest
 from src.classes import Category, Product, SmartPhone, LawnGrass
 
 
-@pytest.fixture
-def class_category():
-    return Category('Смартфоны',
+class_category =  Category('Смартфоны',
                     'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни',
                     [{
                         'name': "Samsung Galaxy C23 Ultra",
@@ -14,32 +12,28 @@ def class_category():
                     }])
 
 
-def test_category_init(class_category):
+def test_category_init():
     assert class_category.name == 'Смартфоны'
     assert class_category.description == 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни'
 
 
-def test_category_count_of_category(class_category):
+def test_category_count_of_category():
     assert class_category.count_of_category == 2
 
 
-def test_category_repr(class_category):
+def test_category_repr():
     assert class_category.name == 'Смартфоны'
     assert class_category.description == 'Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни'
 
 
-@pytest.fixture
-def class_products():
-    return Product('Samsung Galaxy C23 Ultra', '256GB, Серый цвет, 200MP камера',
+class_products =  Product('Samsung Galaxy C23 Ultra', '256GB, Серый цвет, 200MP камера',
                    180000.0, 5)
 
 
-@pytest.fixture
-def class_products_add():
-    return Product('Sony', 'мычит', 500.0, 7)
+class_products_add =  Product('Sony', 'мычит', 500.0, 7)
 
 
-def test_add_product(class_products, class_products_add):
+def test_add_product():
     assert isinstance(class_products_add, Product)
     assert class_products.name == 'Samsung Galaxy C23 Ultra'
     assert class_products_add.name == 'Sony'
@@ -49,53 +43,54 @@ def test___add__():
     pass
 
 
-def test_products_init(class_products):
+def test_products_init():
     assert class_products.name == 'Samsung Galaxy C23 Ultra'
     assert class_products.description == '256GB, Серый цвет, 200MP камера'
     assert class_products.quantity == 5
 
 
-def test_products_count_of_products(class_products):
+def test_products_count_of_products():
     assert class_products.count_of_products == 4
 
 
-def test_products_repr(class_products):
+def test_products_repr():
     assert class_products.name == 'Samsung Galaxy C23 Ultra'
     assert class_products.description == '256GB, Серый цвет, 200MP камера'
 
 
-def test_category_str(class_products):
+def test_category_str():
     assert class_products.name == 'Samsung Galaxy C23 Ultra'
     assert class_products.prices == 180000.0
 
 
-def test_print_products(class_products):
+def test_print_products():
     assert class_products.name == 'Samsung Galaxy C23 Ultra'
 
 
-def test_print_categories(class_category):
+def test_print_categories():
     assert class_category.name == 'Смартфоны'
 
 
-@pytest.fixture
-def class_smartphone():
-    return SmartPhone(1000.5, 'TYN-5', 512.0, 'Motorolla',
+
+class_smartphone = SmartPhone(1000.5, 'TYN-5', 512.0, 'Motorolla',
                       'The best', 5000, 9, 'Silver')
 
 
-def test_smartphone_init(class_smartphone):
+def test_smartphone_init():
     assert class_smartphone.efficiency == 1000.5
     assert class_smartphone.model == 'TYN-5'
     assert class_smartphone.color == 'Silver'
 
 
-@pytest.fixture
-def class_lawngrass():
-    return LawnGrass('China', 6.5, 'Kanadka', 'For you',
-                     500, 3, 'Green')
+# @pytest.fixture
+# def class_lawngrass():
+#     return LawnGrass('China', 6.5, 'Kanadka', 'For you',
+#                      500, 3, 'Green')
 
+class_lawngrass = LawnGrass('China', 6.5, 'Kanadka', 'For you',
+#                      500, 3, 'Green')
 
-def test_lawngrass_init(class_lawngrass):
+def test_lawngrass_init():
     assert class_lawngrass.made == 'China'
     assert class_lawngrass.period == 6.5
     assert class_lawngrass.color == 'Green'
